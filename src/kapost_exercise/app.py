a2c3614e2b54f7530ef7561b5a7c7ca73eccdb28
dgrisham <dgrisham@mines.edu>
2019-04-01 21:39:03 -0600

    check whether buckets exist and load

--- src/kapost_exercise/app.py
+++ src/kapost_exercise/app.py
@@ -4,16 +4,39 @@
 import argparse
 import sys
 
+import boto3
+from botocore.exceptions import ClientError
+
 __author__ = "dgrisham"
 __copyright__ = "dgrisham"
 __license__ = "mit"
 
 
-def main(args):
-    cli(args)
+def main(argv):
+    args = cli(argv)
+    src = args.src
+    dst = args.dst
+
+    s3 = boto3.resource("s3")
+
+    try:
+        s3.meta.client.head_bucket(Bucket=src)
+    except ClientError as e:
+        print(f"error loading bucket '{src}': {e}", sys.stderr)
+        exit(1)
+    try:
+        s3.meta.client.head_bucket(Bucket=dst)
+    except ClientError as e:
+        print(f"error loading bucket '{dst}': {e}", sys.stderr)
+        exit(1)
+
+    bsrc = s3.Bucket(src)
+    bdst = s3.Bucket(dst)
+
+    return s3, bsrc, bdst
 
 
-def cli(args):
+def cli(argv):
     """Parse command line parameters
 
     Args:
@@ -24,10 +47,10 @@ def cli(args):
     """
     parser = argparse.ArgumentParser(description="Kapost DevOps Technical Exercise")
     parser.add_argument(
-        "-f", "--from", type=str, help="source S3 bucket", required=True
+        "-f", "--from", dest="src", type=str, help="source S3 bucket", required=True
     )
     parser.add_argument(
-        "-t", "--to", type=str, help="destination S3 bucket", required=True
+        "-t", "--to", dest="dst", type=str, help="destination S3 bucket", required=True
     )
     parser.add_argument(
         "-s",
@@ -36,8 +59,8 @@ def cli(args):
         help="threshold for size of files to copy (in MB)",
         required=True,
     )
-    return parser.parse_args(args)
+    return parser.parse_args(argv)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    s3, bsrc, bdst = main(sys.argv[1:])
